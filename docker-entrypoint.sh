c56e80be7dc832f140520efcf3175a1896afbed4
mhabedinpour <abedinpourmh@gmail.com>
2025-09-21 22:26:33 +0330

    fix: fix docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 echo "Migrating Database..."
 /app/scanner -c /app/configs/config.yml migrate
 
-exec /app/scanner -c /app/configs/config.yml api
+exec /app/scanner -c /app/configs/config.yml scan
